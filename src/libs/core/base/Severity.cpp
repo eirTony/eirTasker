98cedb4c104553f018f78b77bb0751ecd1e8ab95
Tony Otto <totto@eclipseir.com>
2016-12-24 03:18:38 -0800

    Incorporate Severity levels into BasicErrorInfo (#6)

--- src/libs/core/base/Severity.cpp
+++ src/libs/core/base/Severity.cpp
@@ -1,11 +1,30 @@
 #include "Severity.h"
 DEFINE_ENUMERATION(Severity, SEVERITY_ENUM);
 
-#if 0
-Severity::Severity(void) {;}
-
-Severity::Static::Static(void)
+bool Severity::is(const Severity sev) const
 {
-
+    return Enumeration::value() >= sev;
 }
-#endif
+
+Severity Severity::level(void) const
+{
+    Severity sev;
+    int v = value();
+
+    if (false)
+        {;}
+    else if (v >= BaseTrace && v <= MaxTrace)
+        sev = Trace;
+    else if (v >= BaseUser && v <= MaxUser)
+        sev = User;
+    else if (v >= BaseInfo && v <= MaxInfo)
+        sev = Info;
+    else if (v >= BaseWarning && v <= MaxWarning)
+        sev = Warning;
+    else if (v >= BaseCritical && v <= MaxCritical)
+        sev = Critical;
+    else if (v >= BaseFatal && v <= MaxFatal)
+        sev = Fatal;
+
+    return sev;
+}
--- src/libs/core/log/Log.h
+++ src/libs/core/log/Log.h
@@ -2,15 +2,11 @@
 #define LOG_H
 #include "LogLib.h"
 
-#include "../boost/log/Logger.h"
-
 class LOGSHARED_EXPORT Log
 {
 public:
     Log(void);
 #if 0
-private:
-    E2BLog::Logger::CorePtr mpCore =  0;
 #endif
 };
 
